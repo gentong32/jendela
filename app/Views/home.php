4503f3178358d3e91088042595588d55e94ea4dc
Anto Media <antok2000@gmail.com>
2022-07-29 09:13:13 +0700

    commit kedua

---
+++ app/Views/home.php
@@ -0,0 +1,17 @@
+<?= $this->extend('layout/default') ?>
+
+<?= $this->section('titel') ?>
+<title>Home &mdash; Budaya Kita</title>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+    <section class="section">
+        <div class="section-header">
+        <h1>Dashboard</h1>
+        </div>
+
+        <div class="section-body">
+            
+        </div>
+    </section>
+<?= $this->endSection() ?>
